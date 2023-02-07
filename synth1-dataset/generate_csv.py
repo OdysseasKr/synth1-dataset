031652cb766224e7228d14771d6e31f821cd89d2
odysseaskr <odysseas@mx3d.com>
2023-02-07 21:49:22 +0100

    Formatting

--- synth1-dataset/generate_csv.py
+++ synth1-dataset/generate_csv.py
@@ -1,12 +1,14 @@
-from zipfile import ZipFile
-from rarfile import RarFile
-from pathlib import Path
-from typing import Dict, Union, Optional, List
-from dataclasses import dataclass
-import pandas as pd
-from .constants import DEFAULT_VALUES, META_COLS
 import copy
 from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+from zipfile import ZipFile
+
+import pandas as pd
+from rarfile import RarFile
+
+from .constants import DEFAULT_VALUES, META_COLS
 
 
 @dataclass
@@ -14,6 +16,7 @@ class Preset:
     """
     Represents one preset for Synth1 including all parameters
     """
+
     name: str
     color: str
     ver: int
@@ -30,7 +33,7 @@ class Preset:
 
     @staticmethod
     def from_file(file_content: str):
-        """ Creates an instance of Preset from a .sy1 file"""
+        """Creates an instance of Preset from a .sy1 file"""
 
         lines = file_content.split("\n")
         if len(lines) < 2:
@@ -44,9 +47,9 @@ class Preset:
             if l == "":
                 continue
             elif "color" in l:
-                color = lines[1].strip().replace("color=","")
+                color = lines[1].strip().replace("color=", "")
             elif "ver" in l:
-                ver = lines[2].strip().replace("ver=","")
+                ver = lines[2].strip().replace("ver=", "")
             else:
                 control, value = l.split(",")
                 params[int(control)] = int(value)
@@ -55,7 +58,7 @@ class Preset:
         return Preset(name, color, ver, sorted_params)
 
 
-def read_zipbank(file_path: Union[Path,str]) -> List[Preset]:
+def read_zipbank(file_path: Union[Path, str]) -> List[Preset]:
     """Reads a .zip bank and returns an array of Presets"""
     presets: List[Preset] = []
     with ZipFile(file_path, "r") as zObject:
@@ -74,7 +77,7 @@ def read_zipbank(file_path: Union[Path,str]) -> List[Preset]:
     return presets
 
 
-def read_rarbank(file_path: Union[Path,str]) -> List[Preset]:
+def read_rarbank(file_path: Union[Path, str]) -> List[Preset]:
     """Reads a .rar bank and returns an array of Presets"""
     presets: List[Preset] = []
     with RarFile(file_path, "r") as zObject:
@@ -87,35 +90,35 @@ def read_rarbank(file_path: Union[Path,str]) -> List[Preset]:
                 if file_content == "":
                     continue
                 preset = Preset.from_file(file_content)
-                if 51 not in preset.params:
-                    import pdb;pdb.set_trace()
+
                 if preset is not None:
                     presets.append(preset)
     return presets
 
 
-def read_folder(folder_path: Union[Path,str]) -> List[Preset]:
+def read_folder(folder_path: Union[Path, str]) -> List[Preset]:
     """Reads a folder containing multiple banks"""
     folder_path = Path(folder_path)
     presets: List[Preset] = []
-    for bank in folder_path.glob('**/*.rar'):
+    for bank in folder_path.glob("**/*.rar"):
         bank_presets = read_rarbank(bank)
         presets.extend(bank_presets)
 
-    for bank in folder_path.glob('**/*.zip'):
+    for bank in folder_path.glob("**/*.zip"):
         bank_presets = read_zipbank(bank)
         presets.extend(bank_presets)
     return presets
 
 
 def remove_duplicates(df: pd.DataFrame):
+    """Checks the CSV for duplicate entries. Only checks the actual parameters and not meta data"""
     ignored_columns = set(META_COLS)
     cols_to_compare = set(df.columns) - ignored_columns
 
     return df.drop_duplicates(subset=cols_to_compare)
 
 
-def generate_csv(folder_path: Union[Path, str], csv_path: [Path, str]):
+def generate_csv(folder_path: Union[Path, str], csv_path: Union[Path, str]):
     """Given a folder with banks, generates a csv containing all preset parameters"""
     folder_path = Path(folder_path)
     csv_path = Path(csv_path)
